2a1a29760e8b108b9acf7c45bc4bbfaceb0c0161
Amundala <tom.amundala@a2k.africa>
2024-04-28 13:40:58 +0300

    Initial push with tailwind and flowbite

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Button } from "flowbite-react";
+function App() {
+  return (
+    <div className='bg-slate-700'>
+      <Button className="">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
